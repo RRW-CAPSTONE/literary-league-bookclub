5328ae5cbb026cf9e7444efee2854ca77f9450b8
Paul Wagner <p.wagner1120@icloud.com>
2023-03-06 12:07:16 -0600

    starting new repo

---
+++ src/main/java/com/example/literaryleague/LiteraryLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.literaryleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraryLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraryLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/literaryleague/LiteraryLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.literaryleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraryLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
